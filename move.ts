c1b315aa910ae6c0c5117d0a21048fec7a2297cc
Craig <cisenor1@gmail.com>
2018-02-25 15:35:25 -0800

    Prelim

--- move.ts
+++ move.ts
@@ -1,11 +1,11 @@
 import { Regexer } from "./src/models/Regexer";
 
 export function move() {
-    let r = new Regexer();
+    let r = new Regexer(/Formula\.1\.2017x(\d{2})\.([A-Za-z\w\-]+)\.(Race|Qualifying)\./);
     try{
-        r.runOnDirectory("../");
-    }catch(e){
-        console.log(e.Message);
+        r.runOnDirectory("../", "../Formula 1/Season 2017/");
+    }catch(e){  
+        console.log(e.message);
     }
 }
 
--- src/models/Regexer.ts
+++ src/models/Regexer.ts
@@ -1,21 +1,53 @@
-import * as fs from "fs";
+import * as fs from "fs-extra";
+import { Promise } from "bluebird";
 
 export class Regexer {
     public static INVALID_DIRECTORY_ERROR: Error = new Error("Invalid Directory");
-    constructor() {
-
+    private _regexp: RegExp;
+    constructor(regex: RegExp) {
+        this._regexp = regex;
     }
 
-    public runOnDirectory(directory: fs.PathLike): boolean {
-        if (!directory){
-            throw Regexer.INVALID_DIRECTORY_ERROR;
-        } 
-        if (!fs.existsSync(directory)){ 
+    public runOnDirectory(sourceDirectory: fs.PathLike, destDirectory:fs.PathLike): Promise<boolean> {
+        if (!sourceDirectory) {
             throw Regexer.INVALID_DIRECTORY_ERROR;
         }
-        const folders = fs.readdirSync(directory);
-        folders.forEach((f)=>console.log(f));
-        return true;
+        if (!fs.existsSync(sourceDirectory)) {
+            throw Regexer.INVALID_DIRECTORY_ERROR;
+        }
+        const folders = fs.readdirSync(sourceDirectory);
+        return Promise.mapSeries(folders.filter((folder)=>!!folder.match(this._regexp)), (f: string, i, total) => {
+            console.log("Processing folder " + f + " (folder " + (i + 1).toString() + " of " + total.toString() + ")");
+            const results = f.match(this._regexp)
+            if (!results) {
+                return;
+            }
+            let number = results[1];
+            let country = results[2];
+            // Great Britain is spelled Great.Britain. Replace the . with a space.
+            country = country.replace("."," ");
+            const folderName = number + " - " + country;
+            const category = results[3];
+
+            let roundDir = destDirectory + folderName;
+            if (!fs.existsSync(roundDir)) {
+                fs.mkdirSync(roundDir);
+            }
+
+            let catDir = roundDir + "\\" + category;
+            if (!fs.existsSync(catDir)) {
+                fs.mkdirSync(catDir);
+            }
+
+            let files = fs.readdirSync(sourceDirectory + f);
+            return Promise.mapSeries(files, (file, index, length) => {
+                const src = sourceDirectory + f + "\\" + file;
+                const dest = catDir + "\\" + file;
+                console.log("Copying " + file + " to " + dest + " (" + (index + 1).toString() + " of " + length.toString() + ")");
+                return fs.copy(src, dest);
+
+            });
+        }).return(true);
     }
 
     public getMatchNames(): string[] {
